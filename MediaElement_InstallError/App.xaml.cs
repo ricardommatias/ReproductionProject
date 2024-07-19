28d883e4b7bc0699ab4f1e427bea9f53d7a0b231
ricardo.matias <rmatias@grupopie.com>
2024-07-19 18:13:14 +0100

    Added MediaElement_InstallError

---
+++ MediaElement_InstallError/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MediaElement_InstallError
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MediaElement_InstallError/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MediaElement_InstallError
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
